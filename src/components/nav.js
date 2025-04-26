97a2be53b8511d9b061867860f872bb68efae541
piyushkanadje <piyushkanadje@gmail.com>
2025-04-25 20:59:28 -0400

    Resume added

--- src/components/nav.js
+++ src/components/nav.js
@@ -294,7 +294,7 @@ class Nav extends Component {
                 <CSSTransition classNames={fadeDownClass} timeout={timeout}>
                   <div style={{ transitionDelay: `${isHome ? navLinks.length * 100 : 0}ms` }}>
                     <StyledResumeButton
-                      href="https://drive.google.com/file/d/1mokVJfMMnKb9YKavDbafxZARGHY4wM5O/view?usp=sharing"
+                      href="https://drive.google.com/file/d/1XC342QOkW3VmxQv2O_xy9CYSlwlMa0TF/view?usp=drive_link"
                       target="_blank"
                       rel="nofollow noopener noreferrer">
                       Resume
